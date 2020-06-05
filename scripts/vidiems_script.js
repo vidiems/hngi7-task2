b4ca06cd2b4154003de45dae445511609d0acd2d
vidiems <victory_emueje@yahoo.com>
2020-06-05 23:27:27 +0100

    updated email

--- scripts/vidiems_script.js
+++ scripts/vidiems_script.js
@@ -1,6 +1,6 @@
 const name = "Victory Emueje"
 const hng_id = "HNG-06461"
 const email = "victory_emueje@yahoo.com"
-const lang = "JavaScript"
+const language = "JavaScript"
 
-console.log(`Hello World, this is ${name} with HNGi7 ID ${hng_id} using ${lang} for stage 2 task email ${email}`);
+console.log(`Hello World, this is ${name} with HNGi7 ID ${hng_id} using ${language} for stage 2 task email ${email}`);
