2aa03a6c8578e8b06e3728f465046f61ff2fdfa6
vidiems <victory_emueje@yahoo.com>
2020-06-06 00:33:29 +0100

    Update

--- scripts/victory_script.js
+++ scripts/victory_script.js
@@ -1,6 +1,17 @@
-const name = "Victory Emueje"
-const hng_id = "HNG-06461"
-const email = "victory_emueje@yahoo.com"
-const language = "JavaScript"
-
-console.log(`Hello World, this is ${name} with HNGi7 ID ${hng_id} using ${language} for stage 2 task email ${email}`);
+//function that returns my full details as a string 
+const myDetails = () => {
+    //variable to hold my data 
+    
+     const name = 'Victory Emueje',
+           id ='HNG-06461',
+           email = 'victory_emueje@yahoo.com',
+           lang = 'JavaScript';
+   
+          
+          //returns a string containing details 
+    return `Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task`;
+  }
+  
+  //prints this output to the browser
+ console.log(myDetails());
+ 
