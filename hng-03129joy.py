d3e37345424dbb576ac6c639bc737949c84815a3
vidiems <victory_emueje@yahoo.com>
2020-06-06 01:38:41 +0100

    edit

--- hng-03129joy.py
+++
@@ -1,5 +0,0 @@
-email = "fadamakay@gmail.com"
-name = "Kayode Martins Fadesere"
-hng_id = "HNG-03129"
-lang = "Python"
-print(f"Hello World, this is {name} with HNGi7 ID {hng_id} using {lang} for stage 2 task email {email}")
