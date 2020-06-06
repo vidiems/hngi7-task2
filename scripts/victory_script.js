ca2a95f2555de861c40227983beacac47502fa15
vidiems <victory_emueje@yahoo.com>
2020-06-06 02:57:59 +0100

    update
    update

--- scripts/victory_script.js
+++ scripts/victory_script.js
@@ -10,6 +10,5 @@ const myDetails = () => {
     return `Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task`;
   }
   
-  //prints this output to the browser
  console.log(myDetails());
  
